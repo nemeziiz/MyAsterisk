cc56fe735e1432ee4ed6db2829e1c0c5f77bf876
nemeziz <miladb70@gmail.com>
2020-02-07 23:26:58 +0330

    2020/02/07

--- asterisk.py
+++ asterisk.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 # Print Asterisk
 
-import time
 
-chars = r"\|/-"
+from time import sleep
+
 print("----------\r", end="")
+chars = r"\|/-"
 
-for r in range(10):
-    for t in range(5):
+for column in range(10):
+    for field in range(2):
         for char in chars:
             print(char, end="", flush=True)
-            time.sleep(0.1)
+            sleep(0.1)
             print("\b", end="")
     print("*", end="")
-print()
+
+print(" \b", end="")
+
+for column in range(10):
+    for field in range(2):
+        for char in chars:
+            print(char, end="", flush=True)
+            sleep(0.1)
+            print("\b", end="")
+    print(" \b\b", end="")
